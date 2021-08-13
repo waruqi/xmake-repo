b581a6fa771b9b20705d50c17b8b94a364f3996a
ruki <waruqi@gmail.com>
2021-08-13 17:32:53 +0800

    Update xmake.lua

--- packages/g/giflib/xmake.lua
+++ packages/g/giflib/xmake.lua
@@ -8,7 +8,7 @@ package("giflib")
     add_versions("5.2.1", "31da5562f44c5f15d63340a09a4fd62b48c45620cd302f77a6d9acf0077879bd")
 
     add_configs("utils", {description = "Build utility binaries.", default = true, type = "boolean"})
-
+ 
     if is_plat("windows") then
         add_deps("cgetopt")
     end
